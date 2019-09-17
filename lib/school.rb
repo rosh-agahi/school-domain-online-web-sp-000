be8cb0715b2c7f9b22b95442e782d4574be51e30
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-17 20:31:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,11 +9,11 @@ class School
   end 
   
   def add_student(name, grade)
-   if @roster[grade] != nil
-     @roster[grade] << name
-   else 
-     @roster[grade] = name
-   end
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else 
+      @roster[grade] = [name]
+    end
   end
   
 end
