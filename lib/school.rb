c62cc72494580e8ec80463276b372734c575ae85
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-17 19:28:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
 # code here!
+class School 
+  attr_accessor :name 
+  
+  
+  def initialize(name)
+    @name = name
+    roster = Hash.new
+  end
+  
+  def add_student(student_name,grade)
+    roster[grade] << student_name
+  end
+
+  
+end
