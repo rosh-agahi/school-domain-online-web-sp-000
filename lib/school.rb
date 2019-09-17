6d8d3380468aa7f3c584db1614c042d17ec92667
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-17 19:46:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,19 +8,4 @@ class School
     @roster = {}
   end 
   
-  def add_student(name, grade)
-   if @roster[grade]!=NIL
-     @roster[grade] = []
-     else @roster[grade] << [name]
-  end
-  
-  #def grade(grade)
-  #  @roster[grade]
-  #end
-
-  def sort 
-    @roster.each do |key, value|
-      value.sort!
-    end
-  end
 end
